f3b85c3b9cfce92ed5a0af1c0b7d4cd4d0de6212
Kevin Brightwell <kevin.brightwell2@gmail.com>
2016-12-30 20:43:20 -0500

    Add Exercise 1

---
+++ src/main/java/ca/uwo/eng/se2205b/PalindromicPrime.java
@@ -0,0 +1,22 @@
+package ca.uwo.eng.se2205b;
+
+import java.util.Iterator;
+
+/**
+ * Finds prime numbers and checks if they are palindromes.
+ */
+public class PalindromicPrime {
+
+    /**
+     * Creates an iterator that returns prime palindrome numbers.
+     *
+     * @return Non-{@code null} iterator to get palindrome prime numbers.
+     */
+    public Iterator<Integer> palindromeIterator() {
+
+        return null;
+    }
+
+
+
+}
